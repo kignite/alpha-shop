e0da582923310f317cfbd03b24feb897715c158d
kignite <b82732433@hotmail.com.tw>
2022-11-08 19:03:50 +0800

    更新 計算價格function

--- src/components/Main/Cart.jsx
+++ src/components/Main/Cart.jsx
@@ -18,7 +18,7 @@ const products = [
   },
 ]
 
-function RenderCart( products ) {
+function RenderCart(products) {
   const listItems = products.data.map(item =>
     <div className="cartProduct" key={item.id}>
 
@@ -46,51 +46,28 @@ function RenderCart( products ) {
   )
 }
 
+function CaculatePaid(products) {
+  let totalPrice = 0
+  products.data.map(item =>
+    totalPrice += item.price
+  )
+  return totalPrice
+
+}
+
 export default function Cart() {
   return (
     <section className="cart">
       <div className="cartContainer">
         <h3 className="cartTitle">購物籃</h3>
         <RenderCart data={products} />
-        {/* <div className="cartProduct">
-          <div className="cartItem">
-            <img src="https://picsum.photos/300/300?text=2" alt="貓咪乾乾" />
-            <div className="productInfo">
-              <div className="detail">
-                <span>破壞補釘修身牛仔褲</span>
-                <span className="price">$100</span>
-              </div>
-              <div className="quantitySelect">
-                <Minus />
-                <p>1</p>
-                <Plus />
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="cartProduct">
-          <div className="cartItem">
-            <img src="https://picsum.photos/300/300?text=2" alt="貓咪乾乾" />
-            <div className="productInfo">
-              <div className="detail">
-                <span>破壞補釘修身牛仔褲</span>
-                <span className="price">$100</span>
-              </div>
-              <div className="quantitySelect">
-                <Minus />
-                <p>1</p>
-                <Plus />
-              </div>
-            </div>
-          </div>
-        </div> */}
         <div className="shppingPrice">
           <span>運費</span>
           <span>免費</span>
         </div>
         <div className="totalPrice">
           <span>小計</span>
-          <span>$5,298</span>
+          <span>$<CaculatePaid data={products}/></span>
         </div>
       </div>
     </section>
